98c96e24e5ed3fb57208f7ddad0ebe3b689f287e
PetcuStefan <stefanitapetcu@gmail.com>
2026-05-16 23:06:43 +0300

    added some points of interest

--- locations.js
+++ locations.js
@@ -3,25 +3,97 @@ export const locations = [
     title: "Izvorul 'Renașterea'",
     x: 47,
     y: 49,
-    image: "images/tower.jpg",
-    description: `Simbol al iubirii eterne și al durerii părintești.
-
-Ridicat în 1927 în memoria Sissi Ștefanidi.`
+    image: "images/izvor.jpg",
+    description: `Simbol al iubirii eterne și al durerii părintești 
+În spatele acestei denumiri se ascunde una dintre cele mai triste legende ale parcului. Izvorul a fost ridicat de familia ȘTEFANIDI în memoria fiicei lor, Sissi Ștefanidi, care a murit la doar 21 de ani. Statuia înfățișează o femeie (reprezentând mama) care toarnă apă dintr-un ulcior. Povestea spune că această familie, care donase terenul pentru amenajarea parcului, a vrut să transforme durerea în ceva durabil - un monument plin de sensibilitate și grijă. La baza statuii este inscripția: "IZVORUL SISSI - ÎNCHINAT SCUMPEI MELE COPILE SISSI ȘTEFANIDI".
+• Anul inaugurării: 1927
+• Sculptor: Ion C. Dimitriu-Bârlad (1890-1964)
+• Material: Piatră de Bașchioi (soclu din piatră)
+• Localizare: Partea central-vestică, pe aleea principală, vizavi de Rotonda Scriitorilor
+• Notă: Monumentul este înscris în Lista monumentelor istorice 2010 - cod LMI B-III-m-B-20050
+`
   },
 
   {
-    title: "Old Tower",
-    x: 42,
-    y: 61,
-    image: "images/tower.jpg",
-    description: "Ancient defensive tower."
+    title: "Aleea Scriitorilor (Rotonda Scriitorilor)",
+    x: 38,
+    y: 60,
+    image: "images/scriitori.jpg",
+    description: `Panteonul de piatră din inima naturii 
+Ansamblul a fost inaugurat în plin Război Mondial (27 iunie 1943), la inițiativa ministrului Ion Petrovici. Adăpostește 12 busturi ale marilor oameni de litere, amplasate în cerc pe postamente de piatră de Muscel. Busturile reprezintă: Mihai Eminescu, Alexandru Odobescu, Titu Maiorescu, Ion Luca Caragiale, George Coșbuc, Ștefan Octavian Iosif, Ion Creangă, Alexandru Vlahuță, Duiliu Zamfirescu, Bogdan Petriceicu Hașdeu, Nicolae Bălcescu și Vasile Alecsandri. O poveste reală spune că în perioada comunistă, bustul lui Titu Maiorescu a fost înlăturat din motive ideologice (fiind considerat „burghez"), fiind reinstalat mult mai târziu. Tot aici se adunau studenții în perioada interbelică pentru a recita poezii.
+• Anul inaugurării: 27 iunie 1943
+• Peisagist: Arh. Friedrich Rebhuhn
+• Material: Marmură pe socluri de piatră de Muscel
+• Localizare: Partea de vest a parcului, într-o alee circulară cu rază de 30 de metri
+`
   },
-
-  {
-    title: "Castle",
-    x: 70,
+    {
+    title: "Foisorul Muzicii",
+    x: 60,
     y: 30,
-    image: "images/castle.jpg",
-    description: "Main castle area."
-  }
+    image: "images/scriitori.jpg",
+    description: `Inima ritmurilor de epocă și a plimbărilor duminicale 
+Foișorul de fanfară este locul unde, în fiecare duminică din „Belle Époque", bucureștenii îmbrăcați la patru ace veneau să asculte valsuri și marșuri militare. Structura metalică asigura o acustică perfectă, iar sub puntea foișorului se afla un mic spațiu unde tinerii îndrăgostiți își lăsau bilețele de amor ascunse în crăpăturile lemnului.
+• Anul inaugurării: Cca. 1889 (restaurat ulterior)
+• Arhitect: Planurile Serviciului Tehnic Municipal
+• Material: Fier forjat și lemn  
+• Localizare: Zona central-vestică, între Patrulul Central și Lacul Mare`
+
+  },
+      {
+    title: "Terasa Cismigiu (Fostul Restaurant Monte Carlo / Debarcaderul)",
+    x: 56,
+    y: 62,
+    image: "images/debarcader.jpg",
+    description: `Locul unde Caragiale își „vâna" personajele pentru schițe 
+Faimoasa terasă de pe malul lacului a fost epicentrul boemei bucureștene. Istoria literară amintește că I.L. Caragiale stătea adesea la mesele de aici, savurând o bere și notând replici autentice auzite de la cucoanele și politicienii vremii, replici care au ajuns ulterior în operele sale. Clădirea originală a lui Ion Mincu a fost distrusă la Primul Război Mondial, fiind refăcută de Octav Doicescu în stil modernist.
+• Anul primei construcții: Sfârșitul sec. XIX (reconstruită în interbelic)
+• Arhitect: Octav Doicescu (varianta modernistă)
+• Material: Lemn, piatră și beton armat
+• Localizare: Malul nordic al lacului principal
+`
+
+  },
+  {
+    title: "Podul mic",
+    x: 54,
+    y: 44,
+    image: "images/debarcader.jpg",
+    description: `Locul preferat pentru cărțile poștale romantice 
+Spre deosebire de Podul Mare, Podul Mic a fost gândit inițial ca o structură rustică din bârne de lemn, cu aspect nefinisat. Era fundalul favorit pentru fotografii ambulanți din perioada interbelică, care vindeau fotografii la minut cu bucureșteni ce pozau melancolic privind reflexia apei.
+• Anul amenajării: Prima versiune în 1854 (reînnoite periodic)
+• Peisagist: Carl Meyer
+• Material: Lemn brut și elemente metalice discrete
+• Localizare: Zona de nord-est, peste brațele înguste ale lacului
+`
+
+  },
+    {
+    title: "Podul mare",
+    x: 62,
+    y: 61,
+    image: "images/debarcader.jpg",
+    description: `O replică pariziană din beton deasupra apelor Cișmigiului 
+Podul Mare este un monument de inginerie și estetică. Proiectat de legendarul inginer român Elie Radu, podul este inspirat de peisagistica podurilor din Parcul Buttes-Chaumont din Paris. A fost una dintre primele structuri din beton armat decorativ din capitală, finisată să pară din piatră brută și rezistând impecabil la toate cutremurele mari. În martie 2023, podul a fost reabilitat pentru prima dată în istoria sa de peste 100 de ani.
+• Anul construcției: 1913-1914 
+• Inginer / Proiectant: Elie Radu
+• Material: Beton armat finisat cu aspect de piatră brută
+• Localizare: Traversează canalul central dintre cele două lacuri
+• Notă: Prima reabilitare majoră în martie 2023, după 109 ani de existență
+`
+
+  },
+    {
+    title: "Pajistea Centrala",
+    x: 58,
+    y: 91,
+    image: "images/debarcader.jpg",
+    description: `Covorul verde unde „sergenții" dădeau amenzi usturătoare 
+Astazi ne așezăm relaxați pe iarbă, însă până la jumătatea secolului XX, călcatul pe pajiștea din Cișmigiu era strict interzis, conform rigorilor germane impuse în horticultură. Parcul era păzit de sergenți municipali cu mustăți impunătoare care fluierau strident și aplicau amenzi pe loc oricărui cetățean îndrăzneț care părăsea aleea pietruită.
+• Anul stabilizării peluzei: 1910 (în forma actuală)
+• Peisagist: Friedrich Rebhuhn
+• Material: Pajiște englezească cu flori de decor
+• Localizare: Zona dintre Aleea Scriitorilor și lac
+`
+  },
 ]
