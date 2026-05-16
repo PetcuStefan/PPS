99e7404e574cc8109dcdbabf8f03d59d9207631d
PetcuStefan <stefanitapetcu@gmail.com>
2026-05-17 00:07:41 +0300

    added some pictures

--- locations.js
+++ locations.js
@@ -3,7 +3,7 @@ export const locations = [
     title: "Izvorul 'Renașterea'",
     x: 45,
     y: 51,
-    image: "images/izvor.jpg",
+    image: "images/izvor.jpeg",
     description: `Simbol al iubirii eterne și al durerii părintești 
 În spatele acestei denumiri se ascunde una dintre cele mai triste legende ale parcului. Izvorul a fost ridicat de familia ȘTEFANIDI în memoria fiicei lor, Sissi Ștefanidi, care a murit la doar 21 de ani. Statuia înfățișează o femeie (reprezentând mama) care toarnă apă dintr-un ulcior. Povestea spune că această familie, care donase terenul pentru amenajarea parcului, a vrut să transforme durerea în ceva durabil - un monument plin de sensibilitate și grijă. La baza statuii este inscripția: "IZVORUL SISSI - ÎNCHINAT SCUMPEI MELE COPILE SISSI ȘTEFANIDI".
 • Anul inaugurării: 1927
@@ -18,7 +18,7 @@ export const locations = [
     title: "Aleea Scriitorilor (Rotonda Scriitorilor)",
     x: 39,
     y: 62,
-    image: "images/scriitori.jpg",
+    image: "images/scriitori.jpeg",
     description: `Panteonul de piatră din inima naturii 
 Ansamblul a fost inaugurat în plin Război Mondial (27 iunie 1943), la inițiativa ministrului Ion Petrovici. Adăpostește 12 busturi ale marilor oameni de litere, amplasate în cerc pe postamente de piatră de Muscel. Busturile reprezintă: Mihai Eminescu, Alexandru Odobescu, Titu Maiorescu, Ion Luca Caragiale, George Coșbuc, Ștefan Octavian Iosif, Ion Creangă, Alexandru Vlahuță, Duiliu Zamfirescu, Bogdan Petriceicu Hașdeu, Nicolae Bălcescu și Vasile Alecsandri. O poveste reală spune că în perioada comunistă, bustul lui Titu Maiorescu a fost înlăturat din motive ideologice (fiind considerat „burghez"), fiind reinstalat mult mai târziu. Tot aici se adunau studenții în perioada interbelică pentru a recita poezii.
 • Anul inaugurării: 27 iunie 1943
@@ -31,7 +31,7 @@ Ansamblul a fost inaugurat în plin Război Mondial (27 iunie 1943), la inițiat
     title: "Foisorul Muzicii",
     x: 50,
     y: 53,
-    image: "images/foisor.jpg",
+    image: "images/foisor.jpeg",
     description: `Inima ritmurilor de epocă și a plimbărilor duminicale 
 Foișorul de fanfară este locul unde, în fiecare duminică din „Belle Époque", bucureștenii îmbrăcați la patru ace veneau să asculte valsuri și marșuri militare. Structura metalică asigura o acustică perfectă, iar sub puntea foișorului se afla un mic spațiu unde tinerii îndrăgostiți își lăsau bilețele de amor ascunse în crăpăturile lemnului.
 • Anul inaugurării: Cca. 1889 (restaurat ulterior)
@@ -44,7 +44,7 @@ Foișorul de fanfară este locul unde, în fiecare duminică din „Belle Époqu
     title: "Terasa Cismigiu (Fostul Restaurant Monte Carlo / Debarcaderul)",
     x: 56,
     y: 62,
-    image: "images/debarcader.jpg",
+    image: "images/debarcader.jpeg",
     description: `Locul unde Caragiale își „vâna" personajele pentru schițe 
 Faimoasa terasă de pe malul lacului a fost epicentrul boemei bucureștene. Istoria literară amintește că I.L. Caragiale stătea adesea la mesele de aici, savurând o bere și notând replici autentice auzite de la cucoanele și politicienii vremii, replici care au ajuns ulterior în operele sale. Clădirea originală a lui Ion Mincu a fost distrusă la Primul Război Mondial, fiind refăcută de Octav Doicescu în stil modernist.
 • Anul primei construcții: Sfârșitul sec. XIX (reconstruită în interbelic)
@@ -58,7 +58,7 @@ Faimoasa terasă de pe malul lacului a fost epicentrul boemei bucureștene. Isto
     title: "Podul mic",
     x: 54,
     y: 44,
-    image: "images/pod_mic.jpg",
+    image: "images/pod_mic.jpeg",
     description: `Locul preferat pentru cărțile poștale romantice 
 Spre deosebire de Podul Mare, Podul Mic a fost gândit inițial ca o structură rustică din bârne de lemn, cu aspect nefinisat. Era fundalul favorit pentru fotografii ambulanți din perioada interbelică, care vindeau fotografii la minut cu bucureșteni ce pozau melancolic privind reflexia apei.
 • Anul amenajării: Prima versiune în 1854 (reînnoite periodic)
@@ -72,7 +72,7 @@ Spre deosebire de Podul Mare, Podul Mic a fost gândit inițial ca o structură 
     title: "Podul mare",
     x: 62,
     y: 61,
-    image: "images/pod_mare.jpg",
+    image: "images/pod_mare.jpeg",
     description: `O replică pariziană din beton deasupra apelor Cișmigiului 
 Podul Mare este un monument de inginerie și estetică. Proiectat de legendarul inginer român Elie Radu, podul este inspirat de peisagistica podurilor din Parcul Buttes-Chaumont din Paris. A fost una dintre primele structuri din beton armat decorativ din capitală, finisată să pară din piatră brută și rezistând impecabil la toate cutremurele mari. În martie 2023, podul a fost reabilitat pentru prima dată în istoria sa de peste 100 de ani.
 • Anul construcției: 1913-1914 
@@ -87,7 +87,7 @@ Podul Mare este un monument de inginerie și estetică. Proiectat de legendarul 
     title: "Ceasul Floral",
     x: 58,
     y: 91,
-    image: "images/ceas.jpg",
+    image: "images/ceas.jpeg",
     description: `Punctul clasic de întâlnire al îndrăgostiților bucureșteni 
 Ceasul Floral a fost zeci de ani o atracție tehnico-vegetală unică. Bucureștenii își amintesc cu nostalgie de o practică romantic-vandală: tinerii care întârziau la întâlniri dădeau uneori acele ceasului înapoi manual (când nu vedeau paznicii), pentru a avea scuza că „au ajuns la timp". Ceasul are 4 cadrane și este placat cu aur.
 • Anul inaugurării: Cca. 1960 (modernizat periodic)
@@ -99,7 +99,7 @@ Ceasul Floral a fost zeci de ani o atracție tehnico-vegetală unică. Bucureșt
     title: "Statuia lui Mihai Eminescu",
     x: 36,
     y: 58,
-    image: "images/eminescu.jpg",
+    image: "images/eminescu.jpeg",
     description: `Efigia geniului tutelar al poeziei românești 
 Bustul din Cișmigiu face parte dintr-un ansamblu monumental de excepție. Legenda spune că în timpul instalării din 1943, autoritățile au cerut ca Eminescu să fie plasat central, însă arhitecții peisagiști au insistat să fie integrat în cerc, alături de ceilalți scriitori, pentru a simboliza armonia și egalitatea spiritelor literare.
 • Anul inaugurării: 1943
@@ -112,7 +112,7 @@ Bustul din Cișmigiu face parte dintr-un ansamblu monumental de excepție. Legen
     title: "Platanul (Platanii Seculari)",
     x: 54,
     y: 47,
-    image: "images/plantan.jpg",
+    image: "images/plantan.jpeg",
     description: `Giganții verzi care au supraviețuit regilor și războaielor 
 Nu este o statuie, ci un monument al naturii. Platanii din Cișmigiu au fost plantați în timpul primelor amenajări ale parcului, fiind printre cei mai vechi arbori din București. O frumoasă legendă urbană spune că la rădăcina unuia dintre acești platani uriași ar fi fost îngropată o capsulă a timpului de către primii grădinari, care conținea schițele originale ale parcului.
 • Anul plantării: Cca. 1850
@@ -125,7 +125,7 @@ Nu este o statuie, ci un monument al naturii. Platanii din Cișmigiu au fost pla
     title: "La umbră (Zona Chioșcurilor Istorice)",
     x: 48,
     y: 46,
-    image: "images/la_umbra.jpg",
+    image: "images/la_umbra.jpeg",
     description: `Refugiul boemilor și al cititorilor de presă interbelică 
 Această zonă era renumită pentru chioșcurile de unde se cumpăra limonadă proaspătă și „înghețată la gheață". Aici își petreceau amiezile marii gazetari ai vremii, așteptând ca băieții de curse să aducă ultimele tiraje proaspăt scose de sub tipar de la Palatul Universul, aflat la doar câțiva pași distanță, pe strada Brezoianu. Primul chiosc de ziare din București se afla chiar la intrarea parcului dinspre Bulevardul Regina Elisabeta și poate fi admirat și astazi.
 • Anul amenajării: Cca. 1900
@@ -137,7 +137,7 @@ Această zonă era renumită pentru chioșcurile de unde se cumpăra limonadă p
     title: "Roata Mare (Zona Vechiului Patinoar)",
     x: 57,
     y: 73,
-    image: "images/roata_mare.jpg",
+    image: "images/roata_mare.jpeg",
     description: `Distracția bucureștenilor de la cumpăna dintre secole 
 Cișmigiul a fost legat mereu de ideea de loisir. Iarna, lacul înghețat devenea cel mai faimos patinoar natural din România (deschis oficial în 1882 de către Societatea de Patinaj). În iarna anului 1883, lacul din grădină a înghețat și s-au organizat concursuri pe gheață. Denumirea de „Roata Mare" face referire la parcurile de distracții și serbările caritabile cu lumini și muzică organizate periodic pe timp de iarnă. Astazi, iarna, pe lac este amenajat un patinoar artificial în aer liber cu o suprafață de 3000 de metri pătrați și o capacitate de peste 1000 de persoane.
 • Anul primei amenajări (Patinoar): 1882
@@ -150,7 +150,7 @@ Cișmigiul a fost legat mereu de ideea de loisir. Iarna, lacul înghețat devene
     title: "Loc de joacă (Colțul Guvernantelor)",
     x: 46,
     y: 22,
-    image: "images/loc_de_joaca.jpg",
+    image: "images/loc_de_joaca.jpeg",
     description: `Spațiul unde copiii „de bani gata" învățau franceza 
 În perioada interbelică, acest loc de joacă era cartierul general al bonelor nemțoaice și al guvernatelor franțuzoaice aduse de elitele bucureștene pentru a le educa copiii. Aici s-au legat primele prietenii între viitorii mari intelectuali ai țării, sub privirile severe ale „domnișoarelor" care îi puneau să converseze doar în limbi străine.
 • Anul amenajării: Prima modernizare în cca. 1930
@@ -162,7 +162,7 @@ Cișmigiul a fost legat mereu de ideea de loisir. Iarna, lacul înghețat devene
     title: "Grădina Italiană",
     x: 37,
     y: 23,
-    image: "images/gradina_italiana.jpg",
+    image: "images/gradina_italiana.jpeg",
     description: `Armonia geometrică inspirată din Renaștere 
 Grădina Italiană este o capodoperă de simetrie vegetală, cu garduri vii din cimișir perfect tunse și mici terase suprapuse. Legenda urbană spune că peisagistul Carl Meyer a proiectat acest spațiu special pentru a-i aminti primei soții a domnitorului Barbu Știrbei de grădinile Toscanei.
 • Anul amenajării: Cca. 1852
@@ -175,7 +175,7 @@ Grădina Italiană este o capodoperă de simetrie vegetală, cu garduri vii din 
     title: "Locul istoric al Monumentului Gheorghe Panu",
     x: 51,
     y: 59,
-    image: "images/panu.jpg",
+    image: "images/panu.jpeg",
     description: `Statuia veșnic vie în istoria parcului 
 Bustul lui Gheorghe Panu (1848-1910), ziaristul și omul politic de origine moldavă, cunoscut și sub numele de „Semănătorul de idei", a fost ridicat în 1912 și dezvelit în 1912 în Parcul Cișmigiu. Statuia, realizată de sculptorul Gheorghe Horvath, o reprezintă pe Gheorghe Panu în postura unui semănător: cu mâna stângă își ține poala hainei și în mâna dreaptă îndepărtată de corp ține „semințele". La baza piedestalului sunt reprezentate în bronz ziarul „Lupta" și principalele lucrări ale sale. Bustul a fost ridicat de ziarul „Adevărul" prin subscripție publică. MONUMENTUL RĂMÂNE ÎN PARC ȘI ESTE ÎNSCRIS ÎN LISTA MONUMENTELOR ISTORICE.
 • Anul historic: 1912 (Bustul ridicat și dezvelit în 1912)
@@ -188,7 +188,7 @@ Bustul lui Gheorghe Panu (1848-1910), ziaristul și omul politic de origine mold
     title: "Grădina Franceză",
     x: 51,
     y: 23,
-    image: "images/gradina_franceza.jpg",
+    image: "images/gradina_franceza.jpeg",
     description: `Rigoare versailleză și covoare florale spectaculoase 
 Spre deosebire de restul parcului, care este romantic (stil englezesc), Grădina Franceză impune linii drepte, axe paralele și covoare de flori multicolore. În perioada comunistă, aici se realizau celebrele portrete florale gigantice sau stema țării din mii de flori plantate la milimetru.
 • Anul amenajării: Restructurată masiv în 1910
@@ -201,7 +201,7 @@ Spre deosebire de restul parcului, care este romantic (stil englezesc), Grădina
     title: "Monumentul Eroilor Francezi",
     x: 56,
     y: 56,
-    image: "images/eroi.jpg",
+    image: "images/eroi.jpeg",
     description: `Omagiul de marmură adus sacrificiului din tranșee 
 Opus sculptorului Ion Jalea, acest monument este unul dintre cele mai impunătoare monumente din perimetrul parcului. A fost ridicat în memoria ostașilor francezi care au luptat și s-au jertfit pe teritoriul României în Primul Război Mondial. Lucrarea reprezintă un soldat francez căzut pe câmpul de luptă, vegheat de o figură alegorică feminină (o soră de caritate) care se apleacă și îl sărută pe frunte. Statuia impresionează nu doar prin dimensiunile sale sau prin frumusețea artistică, ci și prin povestea din spatele ei. Ion Jalea, autorul operei, a realizat-o cu o singură mână, după ce și-a pierdut brațul stâng la Mărășești, pe front. Pe soclul de piatră se găsesc inscripții în română și franceză: „SOLDAȚILOR FRANȚEI CĂZUȚI PE CÂMPUL DE ONOARE AL PĂMÂNTULUI ROMÂNESC ÎN TIMPUL MARELUI RĂZBOI 1916-1919" și „AUX SOLDATS FRANÇAIS TOMBÉS AU CHAMP D'HONNEUR SUR LE SOL ROUMAIN PENDANT LA GRANDE GUERRE 1916-1919".
 • Anul creării: 1920 (sculptor a realizat-o în París)
@@ -216,7 +216,7 @@ Opus sculptorului Ion Jalea, acest monument este unul dintre cele mai impunătoa
     title: "Monumentul lui Valter Mărăcineanu",
     x: 78,
     y: 46,
-    image: "images/maracineanu.jpg",
+    image: "images/maracineanu.jpeg",
     description: `Eroul independenței de la granița parcului 
 Situat exact la limita de nord-est a parcului, acest monument din bronz îl onorează pe sublocotenentul Valter Mărăcineanu, care a căzut eroic la Grivița în 1877, înfigând drapelul românesc pe redută. El „păzește" una dintre cele mai frumoase și liniștite intrări în Cișmigiu.
 • Anul inaugurării: 1900
@@ -229,7 +229,7 @@ Situat exact la limita de nord-est a parcului, acest monument din bronz îl onor
     title: "Statuia lui Mihail Kogălniceanu",
     x: 26,
     y: 94,
-    image: "images/kogalniceanu.jpg",
+    image: "images/kogalniceanu.jpeg",
     description: `Martorul de bronz care a supraviețuit bombelor din '44 
 Situată în piața circulară de la colțul de sud-vest al parcului, această statuie impunătoare îl arată pe Kogălniceanu într-o atitudine oratorică. O poveste dramatică și reală spune că în timpul bombardamentelor aviației americane din aprilie 1944, o bombă a căzut la doar câțiva metri. Soclul a fost ciuruit de schije, dar statuia a rămas miraculos în picioare.
 • Anul inaugurării: 1936
@@ -242,7 +242,7 @@ Situată în piața circulară de la colțul de sud-vest al parcului, această s
     title: "Intrare Schitu Măgureanu",
     x: 22,
     y: 12,
-    image: "images/schit.jpg",
+    image: "images/schit.jpeg",
     description: `Poarta istorică ridicată pe moșia călugărilor 
 Grădina Cișmigiu s-a născut parțial pe fostele terenuri ale mănăstirii Schitu Măgureanu (ctitorită la 1756). Legendele urbane menționează că sub această intrare ar exista vechi hrube și tunele secrete folosite de călugări în secolele trecute, care făceau legătura cu Biserica și Palatul Crețulescu.
 • Anul amenajării porții: Cca. 1856
@@ -255,7 +255,7 @@ Grădina Cișmigiu s-a născut parțial pe fostele terenuri ale mănăstirii Sch
     title: "Insula (Insula Lebedelor)",
     x: 32,
     y: 18,
-    image: "images/insula.jpg",
+    image: "images/insula.jpeg",
     description: `Micro-grădina zoologică din mijlocul apelor 
 În perioada interbelică, insula centrală era o atracție exotică majoră: adăpostea o mini-grădină zoologică cu lebede albe și negre, pelicani și păuni donați parcului. Îngrijitorul insulei locuia într-o mică colibă ascunsă de sălcii și avea datoria să hrănească păsările doar la ore fixe, spectacol care aduna sute de privitori pe maluri.
 • Anul amenajării: Cca. 1880
@@ -267,7 +267,7 @@ Grădina Cișmigiu s-a născut parțial pe fostele terenuri ale mănăstirii Sch
     title: "Pajiștea Centrală",
     x: 67,
     y: 67,
-    image: "images/pajistea_centrala.jpg",
+    image: "images/pajistea_centrala.jpeg",
     description: `Covorul verde unde „sergenții" dădeau amenzi usturătoare 
 Astazi ne așezăm relaxați pe iarbă, însă până la jumătatea secolului XX, călcatul pe pajiștea din Cișmigiu era strict interzis, conform rigorilor germane impuse în horticultură. Parcul era păzit de sergenți municipali cu mustăți impunătoare care fluierau strident și aplicau amenzi pe loc oricărui cetățean îndrăzneț care părăsea aleea pietruită.
 • Anul stabilizării peluzei: 1910 (în forma actuală)
@@ -280,7 +280,7 @@ Astazi ne așezăm relaxați pe iarbă, însă până la jumătatea secolului XX
     title: "Administrația Parcului",
     x: 31,
     y: 12,
-    image: "images/administratie.jpg",
+    image: "images/administratie.jpeg",
     description: `Clădirea secretelor verzi și a planurilor vieneze 
 Această clădire discretă ascunde birourile celor care au coordonat destinele parcului timp de peste un secol. Aici s-au păstrat registre speciale în care erau notați toți arborii rari din parc, cu fișă proprie de observație, unii dintre ei fiind aduși prin comenzi speciale de la marile grădini botanice din Europa Centrală.
 • Anul construcției: Începutul sec. XX
@@ -293,7 +293,7 @@ Această clădire discretă ascunde birourile celor care au coordonat destinele 
     title: "Piața 'Patrioții' (Sfatul Bătrânilor)",
     x: 35,
     y: 15,
-    image: "images/patriotii.jpg",
+    image: "images/patriotii.jpeg",
     description: `Tribuna politică a bunicilor bucureșteni 
 O denumire pur populară și extrem de savuroasă întâlnită des în folclorul urban bucureștean. Această zonă este locul unde, de decenii, pensionarii se adună zilnic pe bănci pentru a dezbate cu patos „politica mare", știrile sau sportul. În presa și cultura urbană contemporană este numită cu umor „A doua Cameră a Deputaților" sau „Parlamentul din Cișmigiu".
 • Anul apariției fenomenului: Post-1950
